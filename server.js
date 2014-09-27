690455110953ff56b887323e916a1d6520602569
NgaNguyenDuy <kyo1508@gmail.com>
2014-09-27 07:29:32 +0700

    main app file

--- server.js
+++ server.js
@@ -3,13 +3,33 @@
 var express = require('express'),
     app = express(),
     RouterRoot,
-    routes = require('./routes');
-    // users = require('./routes/users'),
-    // posts = require('./routes/posts');
+    config = require('./config'),
+    routes = require('./routes'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    mongoose = require('mongoose');
+
+
+mongoose.connect(config.mongo_url, function(err) {
+    if (err) throw err;
+    console.log('Connect database successeful!!! ');
+});
+
+
+// Using middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'Secret key'
+}));
 
 
 // Config express
-app.set('port', process.env.PORT || 9009);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
@@ -20,7 +40,15 @@ RouterRoot = express.Router();
 
 
 RouterRoot.get('/', routes.index);
-RouterRoot.post('/login', routes.users.login);
+RouterRoot.get('/login', routes.users.login);
+RouterRoot.post('/login', routes.users.authen);
+
+
+RouterRoot.get('/register', routes.users.register);
+RouterRoot.post('/register', routes.users.reguser);
+
+RouterRoot.get('/logout', routes.users.logout);
+
 
 RouterRoot.post('/posts', routes.posts.create);
 RouterRoot.get('/posts', routes.posts.read);
@@ -36,6 +64,6 @@ RouterRoot.all('*', function(req, res) {
 app.use('/', RouterRoot);
 
 
-app.listen(app.get('port'), function() {
-    console.log('Express server running at ' + app.get('port'));
+app.listen( config.port, function() {
+    console.log('Express server running at ' + config.port);
 });
