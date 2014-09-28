ccd90c661eb1158328a34e6f19d03bc10ecec0ef
NgaNguyenDuy <kyo1508@gmail.com>
2014-09-29 06:59:46 +0700

    add route with create categories

---
+++ routes/categories.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function() {
+    
+    var Cate = require('../models/category');
+    
+    var add = function(req, res) {
+        var cat = new Cate();
+        // cat.cat_name = req.body.cate_name;
+        
+        cat.cat_name =  req.body.cat_name;
+        
+        cat.save(function(err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json({message: 'Added category successeful!'});
+            }
+        });
+        // console.log(cat);
+        // console.log(req.body.cate_name);
+        // res.json('add categories');
+    };
+    
+    return {
+        add: add
+    };
+}();
